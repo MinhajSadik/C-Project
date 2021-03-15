d74e20cddc73153f7dfca6e3f17240043ce3ca10
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-15 19:12:04 +0600

    some working of array

--- array-concept/arrange_number.c
+++ array-concept/arrange_number.c
@@ -3,28 +3,34 @@ int main()
 {
     int i, j, arr[10], temp;
 
-    for (i = 0; i < 10; i++)
-    {
+    for (i = 0; i < 10; i++){
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < 10; i++)
-    {
+    for (i = 0; i < 10; i++){
 
-        for (j = 0; j < 10; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
+        for (j = 0; j < 10; j++){
+
+            if (arr[j] > arr[j + 1]){
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
-    for (i = 0; i < 10; i++)
-    {
+    for (i = 0; i < 10; i++){
         printf("%d\n", arr[i]);
     }
 
     return 0;
+}
+#include <stdio.h>
+int main ()
+{
+    int i, j, arr[10], temp;
+    for ( i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
 }
